c877eea6ad26c99ebb9a73a4174fce60eb728e40
maxim.stoma <maksim.stoma@mail.ru>
2022-06-16 00:57:00 +0300

    task066 solved

---
+++ task066_sumOfNumbersOfTheRange/Program.cs
@@ -0,0 +1,15 @@
+﻿int SumBetweenMN(int firstNumber, int secondNumber)
+{
+    if (firstNumber == secondNumber)
+    {
+        return firstNumber;
+    }
+    return firstNumber + SumBetweenMN(firstNumber + 1, secondNumber);
+}
+Console.Write("Введите первое число: ");
+int leftBorder = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int rightBorder = Convert.ToInt32(Console.ReadLine());
+int sum = SumBetweenMN(leftBorder, rightBorder);
+Console.WriteLine(sum);
+Console.ReadLine();
