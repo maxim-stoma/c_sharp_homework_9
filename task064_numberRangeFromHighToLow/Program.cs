3c285a0e7518885fd8d4090e333c1e63df437909
maxim.stoma <maksim.stoma@mail.ru>
2022-06-16 00:44:24 +0300

    task064 solved

---
+++ task064_numberRangeFromHighToLow/Program.cs
@@ -0,0 +1,22 @@
+﻿void NaturalNumbersMN(int firstNumber, int secondNumber)
+{
+    if (firstNumber + 1 == secondNumber)
+    {
+        return;
+    }
+    else
+    {
+        Console.Write(firstNumber);
+        if (firstNumber > secondNumber)
+        {
+            Console.Write(", ");
+        }
+        NaturalNumbersMN(firstNumber - 1, secondNumber);
+    }
+}
+Console.Write("Введите первое число: ");
+int leftBorder = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int rightBorder = Convert.ToInt32(Console.ReadLine());
+NaturalNumbersMN(leftBorder, rightBorder);
+Console.ReadLine();
